b0e00b77eaf27abc3725e21c4265fe635d6c74ef
p3cunha <pcls_@hotmail.com>
2022-06-04 13:39:33 -0300

    Interface created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, VERSION } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
-import { LazyImport, LazyImports } from './dynamic-compoents/dynamic-item';
 import { DynamicService } from './dynamic.service';
 
 @Component({
@@ -8,16 +7,10 @@ import { DynamicService } from './dynamic.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements LazyImports {
+export class AppComponent {
   name = 'Angular ' + VERSION.major;
   selectedComponent = new BehaviorSubject('');
   lazyImports = this.adService.getLazyImports();
-  getLazyImports() {
-    return [
-      new LazyImport('goodbye', import('./good-bye.component'), 'Arlequim'),
-      new LazyImport('hello', import('./hello.component'), 'Anastácia'),
-    ];
-  }
 
   constructor(private adService: DynamicService) {}
 }
